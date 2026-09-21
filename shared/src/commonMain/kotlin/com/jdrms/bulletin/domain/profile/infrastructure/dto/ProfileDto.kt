ef3d12d6cbc527067f142c5acff3a61339bacb82
JacobAyoub <100238116+JacobAyoub@users.noreply.github.com>
2026-09-21 13:42:54 -0700

    Bulletin 38 create and populate seed data (#19)
    * Uploaded SeedData
    
    * Automated SeedData Population
    
    - SeedData now can be automatically populated
    - Fixed Review created_At to be a timestamp rather than milliseconds

--- shared/src/commonMain/kotlin/com/jdrms/bulletin/domain/profile/infrastructure/dto/ProfileDto.kt
+++ shared/src/commonMain/kotlin/com/jdrms/bulletin/domain/profile/infrastructure/dto/ProfileDto.kt
@@ -29,7 +29,7 @@ data class ReviewDto(
     @SerialName("reviewee_id") val revieweeId: String,
     @SerialName("score") val score: Int,
     @SerialName("comment") val comment: String,
-    @SerialName("created_at_millis") val createdAtMillis: Long = 0L
+    @SerialName("created_at") val createdAt: String = ""
 )
 
 @Serializable
@@ -37,6 +37,5 @@ data class ReviewInsertDto(
     @SerialName("reviewer_id") val reviewerId: String,
     @SerialName("reviewee_id") val revieweeId: String,
     @SerialName("score") val score: Int,
-    @SerialName("comment") val comment: String,
-    @SerialName("created_at_millis") val createdAtMillis: Long = 0L
+    @SerialName("comment") val comment: String
 )
